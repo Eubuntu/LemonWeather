a6deeea9de7a572f79c233503576737f1a88ac51
zhanghao <zhanghao@sagereal.com>
2022-05-09 15:54:41 +0800

    主页布局，工具类编写

--- app/src/main/java/com/lw/lemonweather/MainActivity.java
+++ app/src/main/java/com/lw/lemonweather/MainActivity.java
@@ -11,16 +11,26 @@ import com.baidu.location.BDAbstractLocationListener;
 import com.baidu.location.BDLocation;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
+import com.lw.lemonweather.bean.TodayResponse;
+import com.lw.lemonweather.contract.WeatherContract;
 import com.lw.lemonweather.utils.ToastUtils;
+import com.lw.mvplibrary.mvp.MvpActivity;
 import com.tbruyelle.rxpermissions2.RxPermissions;
 
-public class MainActivity extends AppCompatActivity {
+import retrofit2.Response;
 
-    private TextView tvAddressDetail;
+public class MainActivity extends MvpActivity<WeatherContract.WeatherPresenter> implements WeatherContract.IWeatherView {
+
     //权限
     private RxPermissions rxPermissions;
     public LocationClient mLocationClient = null;
     private MyLocationListener myListener = new MyLocationListener();
+
+    private TextView tvInfo;
+    private TextView tvTemperature;
+    private TextView tvLowHeight;
+    private TextView tvCity;
+    private TextView tvOldTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,22 +43,27 @@ public class MainActivity extends AppCompatActivity {
         //权限判断
         rxPermissions = new RxPermissions(this);
         permissionVersion();
-        tvAddressDetail = findViewById(R.id.tv_address_detail);
+        tvInfo = findViewById(R.id.tv_info);
+        tvTemperature = findViewById(R.id.tv_temperature);
+        tvLowHeight = findViewById(R.id.tv_low_height);
+        tvCity = findViewById(R.id.tv_city);
+        tvOldTime = findViewById(R.id.tv_old_time);
     }
 
     //权限判断
-    private void permissionVersion(){
+    private void permissionVersion() {
         //6.0或6.0以上
-        if(Build.VERSION.SDK_INT >= 23){
+        if (Build.VERSION.SDK_INT >= 23) {
             //动态权限申请
             permissionsRequest();
-        }else {//6.0以下
+        } else {//6.0以下
             //发现只要权限在AndroidManifest.xml中注册过，均会认为该权限granted  提示一下即可
-            ToastUtils.showShortToast(this,"你的版本在Android6.0以下，不需要动态申请权限。");
+            ToastUtils.showShortToast(this, "你的版本在Android6.0以下，不需要动态申请权限。");
         }
     }
+
     //
-    private void permissionsRequest(){
+    private void permissionsRequest() {
         rxPermissions.request(Manifest.permission.ACCESS_FINE_LOCATION).subscribe(granted -> {
             if (granted) {
                 startLocation();
@@ -58,21 +73,46 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void startLocation(){
+    private void startLocation() {
         LocationClient.setAgreePrivacy(true);
         try {
             mLocationClient = new LocationClient(this);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        if(mLocationClient != null){
-        mLocationClient.registerLocationListener(myListener);
-        LocationClientOption option = new LocationClientOption();
-        option.setIsNeedAddress(true);
-        option.setNeedNewVersionRgc(true);
-        mLocationClient.setLocOption(option);
-        mLocationClient.start();
+        if (mLocationClient != null) {
+            mLocationClient.registerLocationListener(myListener);
+            LocationClientOption option = new LocationClientOption();
+            option.setIsNeedAddress(true);
+            option.setNeedNewVersionRgc(true);
+            mLocationClient.setLocOption(option);
+            mLocationClient.start();
         }
+    }
+
+    @Override
+    public void getTodayWeatherResult(Response<TodayResponse> response) {
+
+    }
+
+    @Override
+    public void getDataFailed() {
+
+    }
+
+    @Override
+    public void initData(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected WeatherContract.WeatherPresenter createPresent() {
+        return null;
     }
 
     /**
@@ -93,7 +133,6 @@ public class MainActivity extends AppCompatActivity {
             String district = bdLocation.getDistrict();
             String street = bdLocation.getStreet();
             String locationDescribe = bdLocation.getLocationDescribe();
-            tvAddressDetail.setText(addrStr);
         }
     }
 }
